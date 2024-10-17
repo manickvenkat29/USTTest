7e59b4564f830041f3dbbb82dd8e02c67c01a9e9
Manickam <manickam@xpayback.com>
2024-10-17 21:44:03 +0530

    Initial Commit

---
+++ USTTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  USTTest
+//
+//  Created by Manickam on 17/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
